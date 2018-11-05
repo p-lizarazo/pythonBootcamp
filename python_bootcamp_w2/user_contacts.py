9842b24eb8fa846e21be648c81e8b5f79f6d42b7
Santiago <p-lizarazo@javeriana.edu.co>
2018-11-04 23:48:12 -0500

    Stable version with all requirements

--- python_bootcamp_w2/user_contacts.py
+++ python_bootcamp_w2/user_contacts.py
@@ -26,23 +26,21 @@ class Contact:
                                                                                  self.email, self.phones)
 
     def update_contact(self, **fields):
-        pass
-    """
-    
-    def update_contact(self, **fields):
         if "name" in fields:
             self.name = fields["name"]
-        if "last_name" in field:
+        if "last_name" in fields:
             self.last_name = fields["last_name"]
         if "age" in fields:
             self.age = fields["age"]
         if "email" in fields:
             self.email = fields["email"]
-        if "phone" in fields:
-            self.phones[ fields["phone"] ]
-
-        # Later
-    """
+        if "phones" in fields:
+            if "type" in fields:
+                self.phones[fields["phones"]] = fields["type"]
+            if "phone_num" in fields:
+                _temp = self.phones[fields["phones"]]
+                del self.phones[fields["phones"]]
+                self.phones[fields["phone_num"]] = _temp
 
 
 class User:
@@ -103,6 +101,15 @@ def show_menu_1():
 
 
 def show_menu_3():
+    print("===============")
+    print("What do you want to modify")
+    print("1. Name")
+    print("2. Last name")
+    print("3. Age")
+    print("4. Email")
+    print("5. Phone numbers")
+    print("6. Don't modify")
+    print("===============")
     pass
 
 
@@ -135,22 +142,67 @@ def handle_menu_2(user):
     print("==============")
     _option = ""
     _dict = {}
-    while(_option != "n"):
+    while _option != "n":
         _key = int(input("Input phone number: "))
         _val = input("Input number type: ")
-        _dict[_key]=_val
+        _dict[_key] = _val
         _option = input("Do you want to add another number [y/n]: ")
 
-    user.create_contact(_name,_last_name,_age,_email,_dict)
+    user.create_contact(_name, _last_name, _age, _email, _dict)
     pass
 
-#Handle_menu_3
+
+def handle_menu_3(user):
+    print("===============")
+    for i, e in enumerate(user.contact_list):
+        print("{}. {}".format(i, e))
+
+    _option = int(input("Input contact index to modify: "))
+    contact = user.contact_list[_option]
+    while _option != 6:
+        show_menu_3()
+        _option = int(input("Input option: "))
+        if _option == 1:
+            contact.update_contact(name=input("Input new name: "))
+            pass
+        if _option == 2:
+            contact.update_contact(last_name=input("Input new last name: "))
+            pass
+        if _option == 3:
+            contact.update_contact(age=int(input("Input new age: ")))
+            pass
+        if _option == 4:
+            contact.update_contact(email=input("Input new email: "))
+            pass
+        if _option == 5:
+            print("===============")
+            for e in contact.phones:
+                print(e)
+            _option1 = int(input("Which phone do you want to modify or a new number to add a new one: "))
+            if _option1 not in contact.phones:
+                contact.update_contact(phones=_option1, type=input("Phone type: "))
+                continue
+
+            _temps = input("Do you wanna modify the type or number [type/number]: ")
+
+            if _temps == "number":
+                _data = int(input("Input new phone number: "))
+                contact.update_contact(phones=_option1, phone_num=_data)
+            else:
+                _data = input("Input new type: ")
+                contact.update_contact(phones=_option1, type=_data)
+            print("===============")
+            pass
+
+    print("===============")
+    pass
+
 
 def handle_menu_4(user):
     print("==============")
     _l = [x for x in user.contact_list if not x.hidden]
     for i, e in enumerate(_l):
-        print("{}. {}".format(i,e))
+        print("{}. {}".format(i, e))
     _option = int(input("Input number of contact you want to hide: "))
     _l[_option].hidden = True
     print("==============")
@@ -172,7 +224,7 @@ def handle_menu_5(user):
 def handle_menu_6():
     print("===============")
     s = input("Input File name to load (without extension): ")
-    s+=".pickle"
+    s += ".pickle"
     print(s)
     f2 = open(s, "rb")
     from_file = f2.read()
@@ -183,14 +235,6 @@ def handle_menu_6():
 
 
 u = User()
-
-"""
-u.create_contact("Juan","Perez",10,"JP@hotmail.com", { 123:"home", 321:"office"})
-u.create_contact("Jonhatan","Florez",20,"JF@hotmail.com", {3123:"home"})
-u.create_contact("Pedro","Ramirez",30,"PR@gmail.com",{})
-
-"""
-
 option = 0
 while option != 7:
     show_menu()
@@ -198,13 +242,13 @@ while option != 7:
     if option == 1:
         show_menu_1()
         option_1 = int(input("Input number option "))
-        handle_menu_1(u,option_1)
+        handle_menu_1(u, option_1)
         pass
     if option == 2:
         handle_menu_2(u)
         pass
     if option == 3:
-        #missing modified
+        handle_menu_3(u)
         pass
     if option == 4:
         handle_menu_4(u)
