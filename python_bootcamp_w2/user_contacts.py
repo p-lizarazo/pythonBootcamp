31f8dc27f050f573d753666e6231cc082e5b2b62
Santiago <p-lizarazo@javeriana.edu.co>
2018-11-04 21:20:29 -0500

    list contacts, create contacts, hide contacts complete

--- python_bootcamp_w2/user_contacts.py
+++ python_bootcamp_w2/user_contacts.py
@@ -1,45 +1,192 @@
 from datetime import datetime
 
-class Phone:
-    """
-    Define instances of contact number phones with its type
-    """
-    def __init__(self,type="casa",number):
-        self.number=number
-        self.type=type
-        pass
 
 class Contact:
     """
     Define instances of User contacts
     """
-    def __init__(self,name,last_name,age,email):
+
+    def __init__(self, name, last_name, age, email, phones):
         self.hidden = False
         self.creation_date = datetime.now()
         self.name = name
         self.last_name = last_name
         self.age = age
         self.email = email
-        self.Phones = set()
+        # phones is a dictionary with key=phone_number and value=number_type such as "home","office" e.t.c
+        self.phones = phones
         pass
 
-class User:
-    """
-    Define the instace of an user
-    """
-    def __init__(self):
-        self.contact_list=[]
-        pass
+    def __lt__(self, other):
+        return self.creation_date < other.creation_date
+
+    def __str__(self):
+        return "Name: {}, Last name: {}, Age: {}, Email: {}, Phones: {} ".format(self.name, self.last_name, self.age,
+                                                                                 self.email, self.phones)
 
     def update_contact(self, **fields):
         pass
+    """
+    
+    def update_contact(self, **fields):
+        if "name" in fields:
+            self.name = fields["name"]
+        if "last_name" in field:
+            self.last_name = fields["last_name"]
+        if "age" in fields:
+            self.age = fields["age"]
+        if "email" in fields:
+            self.email = fields["email"]
+        if "phone" in fields:
+            self.phones[ fields["phone"] ]
 
-    def hide_contact(self,user):
+        # Later
+    """
+
+
+class User:
+    """
+    Define the instance of an user
+    """
+
+    def __init__(self):
+        self.contact_list = []
         pass
 
-    def create_contact(self,):
+    def update_contact(self, index, **fields):
+        contact = self.contact_list[index]
+        contact.update_contact(fields)
         pass
 
+    def hide_contact(self, index):
+        contact = self.contact_list[index]
+        contact.hidden = True
+        pass
+
+    def create_contact(self, name, last_name, age, email, phones):
+        new_user = Contact(name, last_name, age, email, phones)
+        self.contact_list.append(new_user)
+        pass
+
+    def sort_by_date_and_visible(self):
+        sorted_list = sorted([e for e in self.contact_list if not e.hidden])
+        return sorted_list
+
+    def __get_contact(self, index):
+        return self.contact_list[index]
 
 
+def show_menu():
+    print("==============")
+    print("Welcome User:")
+    print("Options:")
+    print("1. List contacts")
+    print("2. Create new contact")
+    print("3. Update existing contact")
+    print("4. Hide contact")
+    print("5. Save all contacts")
+    print("6. Load Contacts")
+    print("7. Quit")
+    print("==============")
+    pass
 
+
+def show_menu_1():
+    print("===============")
+    print("Options")
+    print("1. List contacts")
+    print("2. List contacts sorted by date creation")
+    print("3. List hidden contacts")
+    print("==============")
+    pass
+
+
+def show_menu_3():
+    pass
+
+
+def show_menu_5():
+    pass
+
+
+def show_menu_6():
+    pass
+
+
+def handle_menu_1(user,option):
+    print("==============")
+    if option == 1:
+        for i in user.contact_list:
+            if not i.hidden:
+                print(i)
+        pass
+    if option == 2:
+        for i in user.sort_by_date_and_visible():
+            print(i)
+        pass
+    if option == 3:
+        for i in user.contact_list:
+            if i.hidden:
+                print(i)
+        pass
+    print("==============")
+    pass
+
+
+def handle_menu_2(user):
+    print("==============")
+    _name = input("Input name: ")
+    _last_name = input("Input Last name: ")
+    _age = int(input("Input age: "))
+    _email = input("Input email: ")
+    print("==============")
+    _option = ""
+    _dict = {}
+    while(_option != "n"):
+        _key = int(input("Input phone number: "))
+        _val = input("Input number type: ")
+        _dict[_key]=_val
+        _option = input("Do you want to add another number [y/n]: ")
+
+    user.create_contact(_name,_last_name,_age,_email,_dict)
+    pass
+
+
+def handle_menu_4(user):
+    print("==============")
+    _l = [x for x in u.contact_list if not x.hidden]
+    for i, e in enumerate(_l):
+        print("{}. {}".format(i,e))
+    _option = int(input("Input number of contact you want to hide: "))
+    _l[_option].hidden = True
+    print("==============")
+    pass
+
+
+u = User()
+u.create_contact("Juan","Perez",10,"JP@hotmail.com", { 123:"home", 321:"office"})
+u.create_contact("Jonhatan","Florez",20,"JF@hotmail.com", {3123:"home"})
+u.create_contact("Pedro","Ramirez",30,"PR@gmail.com",{})
+
+option = 0
+while option != 7:
+    show_menu()
+    option = int(input("Input number option "))
+    if option == 1:
+        show_menu_1()
+        option_1 = int(input("Input number option "))
+        handle_menu_1(u,option_1)
+        pass
+    if option == 2:
+        handle_menu_2(u)
+        pass
+    if option == 3:
+
+        pass
+    if option == 4:
+        handle_menu_4(u)
+        pass
+    if option == 5:
+        pass
+    if option == 6:
+        pass
